dee0ce37d74591ca21071f1bf7ae7f760d0e96ed
Steven Moseni <smoseni@hotmail.com>
2021-03-14 22:55:41 -0700

    created inquirer prompts

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const inquirer = require("inquirer");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager")
+const Manager = require("./lib/Manager");
+const Employee = require("./lib/Employee");
+
+const fs = require("fs");
 
 // --notes from tutoring session--
 // have inquirer prompts that will ask user to builder engineer, intern, manager
@@ -11,3 +14,24 @@ const Manager = require("./lib/Manager")
 // have an exit function 
 // when you run exit function, check to see if output folder exists, if it doesnt create it. 
 // fs write file sync that will take the result of using html render function that you import to make your html file
+
+function runInquirer() {
+    const promptArray = [{
+        type: "input",
+        message: "What is your name?",
+        name: "name"
+    },{
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    },{
+        type: "input",
+        message: "What is your ID?",
+        name: "id"
+    }, {
+        type: "list",
+        message: "What is your title",
+        choices: ["Manager", "Engineer", "Intern"],
+        name: "title"
+    }];
+}
